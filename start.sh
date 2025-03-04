0039a7cbe7f0e35da8e23d3154af1de88c1306c4
jcalvarezj <jcalvarezj@gmail.com>
2025-03-04 11:53:08 -0500

    fix: fix docker-compose and improve start script

--- start.sh
+++ start.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 # Provisional startup of infrastructure (requires already downloaded
 # and cached images on a first run of `docker-compose up` or `docker-compose build`)
+
+if [ ! -d "venv" ]; then
+    echo "Virtual environment not found. Creating and installing dependencies..."
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+else
+    echo "Activating found virtual environment..."
+    source venv/bin/activate
+fi
+
 docker-compose up schema-registry -d
 echo "Waiting a bit for Kafka to start..."
-sleep 35
+sleep 40
 echo "Initializing Kafka topics and schemas"
 python init_kafka.py
 docker-compose up ksqldb -d
 echo "Waiting a bit for KSQLDB to start..."
-sleep 35
+sleep 40
 echo "Initializing KSQLDB Streams"
 source init_ksql.sh
+echo "Starting the rest of the infrastructure..."
 docker-compose up --build
+deactivate
